0b1bfe3ab608ae76ab8ec67fbee155326b0e453b
Roman Kolodnitskyi <roman.kolodnitskyi@makeup.com.ua>
2024-12-14 15:12:31 +0200

    solution of firts task added;

---
+++ 1/Program.cs
@@ -0,0 +1,23 @@
+﻿var firstColumn = new List<int>();
+var secondColumn = new List<int>();
+var lines = File.ReadLines("./input.txt");
+
+foreach (var line in lines)
+{
+    //each line is a string with two numbers separated by a 1-4 spaces or tabs
+    var lineParts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    firstColumn.Add(int.Parse(lineParts[0]));
+    secondColumn.Add(int.Parse(lineParts[1]));
+}
+
+firstColumn.Sort();
+secondColumn.Sort();
+
+int totalColumnsDistance = 0;
+
+for (int i = 0; i < firstColumn.Count; i++)
+{
+    totalColumnsDistance += Math.Abs(firstColumn[i] - secondColumn[i]);
+}
+
+Console.WriteLine("Total distance between columns: " + totalColumnsDistance);
